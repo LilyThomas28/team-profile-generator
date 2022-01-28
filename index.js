ddb791178bf83e11ec54e3579854fc7fa2b9fc24
Lily Thomas <lilylynettethomas@gmail.com>
2022-01-27 16:59:16 -0800

    Made question prompts

--- index.js
+++ index.js
@@ -6,92 +6,135 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-// Array for team
+// Array for team and id
 const team = [];
+const id = [];
 // Prompt for Engineer
-const managerPromt = [
-    {
-        name: "name",
-        type: "input",
-        message: "Enter Team Manager Name:",
-    },
-    {
-        name: "id",
-        type: "input",
-        message: "Enter Employee ID:",
-    },
-    {
-        name: "email",
-        type: "input",
-        message: "Enter Manager email address:",
-    },
-    {
-        name: "officeNumber",
-        type: "input",
-        message: "Enter office number:",
-    },
-    {
-        name: "nextMember",
-        type: "input",
-        choices: ["Add Engineer", "Add Intern", "Team Complete!"],
-        message: "What would you like to do next?",
-    },
-];
+function managerPrompt() {
+    inquirer
+        .prompt([
+        {  
+            name: "name",
+            type: "input",
+            message: "Enter Team Manager Name:",
+        },
+        {  
+            name: "id",
+            type: "input",
+            message: "Enter Employee ID:",
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Enter Manager email address:",
+        },
+        {
+            name: "officeNumber",
+            type: "input",
+            message: "Enter office number:",
+        },
+        {
+            name: "nextMember",
+            type: "input",
+            choices: ["Add Engineer", "Add Intern", "Team Complete!"],
+            message: "What would you like to do next?",
+        },
+    ])
+    .then((results) => {
+        const manager = new Manager(
+            results.name,
+            results.id,
+            results.email,
+            results.officeNumber,
+        );
+        team.push(manager);
+        id.push(results.id);
+        addTeam();
+    });
+}
 // Promt for Intern
-const internPromt = [
-    {
-        name: "name",
-        type: "input",
-        message: "Enter Intern Name:",
-    },
-    {
-        name: "id",
-        type: "input",
-        message: "Enter Employee ID:",
-    },
-    {
-        name: "email",
-        type: "input",
-        message: "Enter Intern email address:",
-    },
-    {
-        name: "school",
-        type: "input",
-        message: "Enter Intern's Educational Institution:",
-    },
-    {
-        name: "nextMember",
-        type: "input",
-        choices: ["Add Engineer", "Add Intern", "Team Complete!"],
-        message: "What would you like to do next?",
-    },
-];
+function internPromt() {
+    inquirer
+        .prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter Intern Name:",
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Enter Employee ID:",
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Enter Intern email address:",
+        },
+        {
+            name: "school",
+            type: "input",
+            message: "Enter Intern's Educational Institution:",
+        },
+        {
+            name: "nextMember",
+            type: "input",
+            choices: ["Add Engineer", "Add Intern", "Team Complete!"],
+            message: "What would you like to do next?",
+        },
+    ])
+    .then((results) => {
+        const intern = new Intern(
+            results.name,
+            results.id,
+            results.email,
+            results.school,
+        );
+        team.push(intern);
+        id.push(results.id);
+        addTeam();
+    });
+}
 // Promt for Manager
-const engineerPromt = [
-    {
-        name: "name",
-        type: "input",
-        message: "Enter Engineer Name:",
-    },
-    {
-        name: "id",
-        type: "input",
-        message: "Enter Employee ID:",
-    },
-    {
-        name: "email",
-        type: "input",
-        message: "Enter Engineer email address:",
-    },
-    {
-        name: "github",
-        type: "input",
-        message: "Enter Engineer's Github Username:",
-    },
-    {
-        name: "nextMember",
-        type: "input",
-        choices: ["Add Engineer", "Add Intern", "Team Complete!"],
-        message: "What would you like to do next?",
-    },
-];
+function engineerPromt() {
+    inquirer
+        .prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter Engineer Name:",
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Enter Employee ID:",
+        },
+        { 
+            name: "email",
+            type: "input",
+            message: "Enter Engineer email address:",
+        },
+        {
+            name: "github",
+            type: "input",
+            message: "Enter Engineer's Github Username:",
+        },
+        {
+            name: "nextMember",
+            type: "input",
+            choices: ["Add Engineer", "Add Intern", "Team Complete!"],
+            message: "What would you like to do next?",
+        },
+    ])
+    .then((results) => {
+        const engineer = new Engineer(
+            results.name,
+            results.id,
+            results.email,
+            results.github,
+        );
+        team.push(engineer);
+        id.push(results.id);
+        addTeam();
+    });
+}
